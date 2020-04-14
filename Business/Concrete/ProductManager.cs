648b74a34e2c2946ad68536192ec02479526737c
Uğur Uyar <uguruyar@protonmail.com>
2020-04-14 13:54:07 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,96 @@
+﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
+using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Transaction;
+using Core.Aspects.Autofac.Validation;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Core.Aspects.Autofac.Exception;
+using Core.Aspects.Autofac.Logging;
+using Core.Aspects.Autofac.Performance;
+using Core.CrossCuttingConcerns.Logging.Log4Net.Loggers;
+using Core.Utilities.Business;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        [ValidationAspect(typeof(ProductValidator), Priority = 1)]
+        //[CacheRemoveAspect("IProductService.Get")]
+        public IResult Add(Product product)
+        {
+            IResult result = BusinessRules.Run(CheckIfProductNameExists(product.ProductName));
+            if (result != null)
+            {
+                return result;
+            }
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
+        }
+
+        private IResult CheckIfProductNameExists(string productName)
+        {
+            if (_productDal.Get(p => p.ProductName == productName) != null)
+            {
+                return new ErrorResult(Messages.ProductNameAlreadyExists);
+            }
+
+            return new SuccessResult();
+        }
+
+        public IResult Update(Product product)
+        {
+            _productDal.Update(product);
+            return new SuccessResult(Messages.ProductUpdated);
+        }
+
+        public IResult Delete(Product product)
+        {
+            _productDal.Delete(product);
+            return new SuccessResult(Messages.ProductDeleted);
+        }
+
+        [TransactionScopeAspect]
+        public IResult TransactionalOperation(Product product)
+        {
+            _productDal.Update(product);
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductUpdated);
+        }
+
+        public IDataResult<Product> GetById(int productId)
+        {
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
+        }
+
+        [PerformanceAspect(5)]
+        public IDataResult<List<Product>> GetList()
+        {
+            Thread.Sleep(5000);
+            return new SuccessDataResult<List<Product>>(_productDal.GetList().ToList());
+        }
+
+        //[SecuredOperation("Product.List,Admin")]
+        [CacheAspect(10)]
+        [LogAspect(typeof(FileLogger))]
+        //[LogAspect(typeof(DatabaseLogger))]
+        public IDataResult<List<Product>> GetListByCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetList(p => p.CategoryId == categoryId).ToList());
+        }
+    }
+}
---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Business.Constants
+{
+    public class Messages
+    {
+        public static string ProductAdded = "Product Added Successfully";
+        public static string ProductUpdated = "Product Updated Successfully";
+        public static string ProductDeleted = "Product Deleted Successfully";
+
+        public static string UserNotFound = "User not found.";
+        public static string PasswordError = "Password wrong.";
+        public static string SuccessfulLogin = "Login Successful";
+        public static string UserAlreadyExists = "User Already Exists";
+        public static string UserRegistered = "User Created Successfully";
+        public static string AccessTokenCreated = "Access Token Created Successfully";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+
+        public static string ProductNameAlreadyExists = "Ürün ismi zaten mevcut.";
+    }
+}
